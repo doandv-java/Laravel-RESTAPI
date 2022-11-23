d923e8a6bfbc92a89fac652c20431de1a99eec49
doandv <doandv188@gmail.com>
2022-11-23 13:14:51 +0700

    init project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,5 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(MySQLDatabaseSeeder::class);
-        $this->call(SQLLiteDatabaseSeeder::class);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\AsyncDatabaseController;
-use App\Http\Controllers\AuthorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,13 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
-Route::get("authors", [AuthorController::class, 'index']);
-Route::get("authors/{id}", [AuthorController::class, 'show']);
-Route::post("authors", [AuthorController::class, 'create']);
-Route::put("authors", [AuthorController::class, 'update']);
-Route::delete("authors/{id}", [AuthorController::class, 'delete']);
-Route::post("async-database", [AsyncDatabaseController::class, 'asyncDatabase']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
